df10c445022c8d33a5b14fe99c5ad9ddca01ca5e
Sony? <sony@4morefun.net>
2012-08-29 22:16:00 +0200

    Better documented.

--- examples/prompt.php
+++ examples/prompt.php
@@ -4,9 +4,9 @@ require '../cmd.php';
 
 
 
-$nick = prompt("Enter your nick:");
-$age  = prompt("Enter your age:", "> 0", "???");
-$num  = prompt("Enter a number bigger then 4:", "> 3");
+$nick = prompt("Enter your nick:");                      //Ask something, dont care about input at all
+$age  = prompt("Enter your age:", "> 0", "???");         //Ask something, if the input IS NOT bigger then 0, the value will be set to ???.
+$num  = prompt("Enter a number bigger then 4:", "> 3");  //Ask something untill the condition is fulfilled.
 
 
 e("Hello '$nick'!");
